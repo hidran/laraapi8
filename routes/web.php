18143e0d04defec42b308d8fc545c35cbb357772
Hidran Arias <hidran@gmail.com>
2021-06-18 16:51:27 +0200

    adds userscontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,5 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
+Route::resource('users', UsersController::class);
 require __DIR__.'/auth.php';
