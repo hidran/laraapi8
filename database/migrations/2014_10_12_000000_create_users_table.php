5e1590f2c2e92f7d47865eaa3918879445d7aae1
Hidran Arias <hidran@gmail.com>
2021-06-18 16:22:25 +0200

    creates laravel projects and adds new column

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname', 32)->nullable();
+            $table->char('fiscalcode', 16)->nullable();
+            $table->string('province', 32)->nullable();
+            $table->string('phone', 32)->nullable();
+            $table->smallInteger('age', false, true)->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
