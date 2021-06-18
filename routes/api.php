f19b69994bebc8e74fb17b179372327d55773795
Hidran Arias <hidran@gmail.com>
2021-06-18 19:41:10 +0200

    Adds auth controller for jwt-auth

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class ,'login']);
+    Route::post('logout',  [AuthController::class ,'logout']);
+    Route::post('refresh',  [AuthController::class ,'refresh']);
+    Route::post('me',  [AuthController::class ,'me']);
+
+});
 Route::resource('users', UsersController::class)->middleware('auth:api');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
